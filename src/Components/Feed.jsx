1348c63c0a03646303682ad22ab6f00a278e7405
Zmrts <julesik.zar@yandex.ru>
2023-11-29 15:45:20 +0400

    chat fixed 2

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,5 +1,5 @@
 import { child, onValue, push, ref, update } from "firebase/database";
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import { Context } from "..";
 import { loadContext } from "../App";
 import { Post } from "./Post";
@@ -16,6 +16,8 @@ function Feed() {
     const {user} = useContext(loadContext);
     const {displayName, photoURL} = user;
 
+    const chatRef = useRef(null);
+
     const emptyArrayStyles = {
         display:'flex',
         alignItems:'center',
@@ -27,11 +29,24 @@ function Feed() {
         const postsRef = ref(database, 'posts/');
         onValue(postsRef, (snapshot) => {
             const data = snapshot.val();
-            const dataArray = Object.values(data)
-            setPosts(dataArray);
+            if (data) {
+                const dataArray = Object.values(data)
+                setPosts(dataArray);
+            } else {
+                console.error('Ошибка при получении данных (/posts)')
+            }
+           
         })
     }
 
+    const scrollToEnd = () => {
+        const scrollHeight = chatRef.current.scrollHeight;
+        chatRef.current.scrollTo({
+            top:scrollHeight,
+            behavior:'smooth',
+        })
+        
+    }
     const handleChangeText = (evt) => {
         setMessage(evt.target.value);
    
@@ -50,26 +65,34 @@ function Feed() {
     }
 
     const sendMessage = (userName, message) => {
-        const postData = {
-            author:userName,
-            message:message,
+        if (userName && message) {
+            const postData = {
+                author:userName,
+                message:message,
+            }
+    
+            const newPostKey = push(child(ref(database), 'posts')).key;
+    
+            const updates = {};
+            updates['/posts/' + newPostKey] = postData;
+    
+            return update(ref(database), updates);
+        } else {
+            console.error('Ошибка при отправке сообщения');
         }
-
-        const newPostKey = push(child(ref(database), 'posts')).key;
-
-        const updates = {};
-        updates['/posts/' + newPostKey] = postData;
-
-        return update(ref(database), updates);
     }
 
     useEffect(() => {
         getPosts();
     }, []);
 
+    useEffect(() => {
+        scrollToEnd();
+    } , [posts])
+
     return <div className="feed">
         <div className="feed_content">
-            <ul style={!posts.length ? emptyArrayStyles : {}}  className="feed_chat">
+            <ul ref={chatRef} style={!posts.length ? emptyArrayStyles : {}}  className="feed_chat">
                 {posts.length 
                 ? posts.map((item) => <Post
                 message={item.message} 
