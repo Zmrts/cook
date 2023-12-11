aa57788c5adcb6b78dcc028a9d046e1bf0cd7eba
Zmrts <julesik.zar@yandex.ru>
2023-12-11 15:44:11 +0400

    test menu

--- src/Components/BuregerMenu.jsx
+++ src/Components/BuregerMenu.jsx
@@ -1,71 +1,74 @@
 import { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 function BuregerMenu() {
-  const [isOpen, setIsOpen] = useState();
+  const [isOpen, setIsOpen] = useState(false);
   const [startTouchX, setStartTouchX] = useState(null);
   const [currentX, setCurrentX] = useState(null);
 
-  const pageWidth = window.innerWidth;
-  console.log(pageWidth);
-
   const burgerListRef = useRef(null);
 
-  const onTouch = (evt) => {
-    setStartTouchX(evt.touches[0].pageX);
-  }
-
-  const handleOnTouchEnd = () => {
-    if (startTouchX !== null) {
-        const burgerList = burgerListRef.current;
-        const diffX = (currentX - startTouchX) * -1 > pageWidth/2;
-        if (diffX) {
-            setIsOpen(false);
-        } else {
-            burgerList.style.transform ='translateX(0)';
-            burgerList.style.opacity ='1';
-        }
-        
-    }
-  }
-   useEffect(() => {
-    const burgerList = burgerListRef.current;
-    if (startTouchX !== null) {
-        const diffX =  currentX - startTouchX;
-        requestAnimationFrame(() => {
-            burgerList.style.transform = diffX < 0 ? `translateX(${diffX}px)` : 0;
-            burgerList.style.opacity = 1 + (diffX / pageWidth);
-        })
-       
-    }
-  }, [currentX])
-
-
+  const pageWidth = window.innerWidth;
 
   const openStyles = {
     opacity: 1,
-    transform: 'translateX(0)'
-
-
+    transform: "translateX(0)",
   };
 
   const closeStyles = {
     opacity: 0,
-    transform: 'translateX(-100%)'
-
+    transform: "translateX(-100%)",
   };
 
   const handleClick = () => {
-    const body = document.querySelector('body');
-    if (!isOpen) {
-        body.style.maxHeight = '100vh'
-        body.style.overflowY = 'hidden';
-        setIsOpen(true);
-    } else {
-        body.style.maxHeight = 'unset';
-        body.style.overflowY = 'visible';
-        setIsOpen(false);
+    setIsOpen(!isOpen);
+  };
+
+  const onTouch = (evt) => {
+    setStartTouchX(evt.touches[0].pageX);
+  };
+  const handleClickToLink = () => {
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 50);
+  };
+
+  const handleOnTouchEnd = () => {
+    if (startTouchX !== null) {
+      const burgerList = burgerListRef.current;
+      setTimeout(() => {
+        const diffX = (currentX - startTouchX) * -1 > pageWidth / 2;
+        if (diffX) {
+          setIsOpen(false);
+        } else {
+          burgerList.style.transform = "translateX(0)";
+          burgerList.style.opacity = "1";
+        }
+      }, 20);
     }
   };
+
+  useEffect(() => {
+    const burgerList = burgerListRef.current;
+    if (startTouchX !== null) {
+      const diffX = currentX - startTouchX;
+      requestAnimationFrame(() => {
+        burgerList.style.transform =
+          diffX < 0 ? `translateX(${diffX * 1}px)` : 0;
+        burgerList.style.opacity = 1 + diffX / pageWidth;
+      });
+    }
+  }, [currentX]);
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (isOpen) {
+      body.style.maxHeight = "100vh";
+      body.style.overflowY = "hidden";
+    } else {
+      body.style.maxHeight = "unset";
+      body.style.overflowY = "visible";
+    }
+  }, [isOpen]);
 
   return (
     <div className="burger">
@@ -79,17 +82,17 @@ function BuregerMenu() {
         <div className="burger_icon_item"></div>
       </div>
       <ul
-      onTouchStart={onTouch}
-      onTouchMove={(evt) => setCurrentX(evt.touches[0].pageX)}
-      onTouchEnd={handleOnTouchEnd}
+        onTouchStart={onTouch}
+        onTouchMove={(evt) => setCurrentX(evt.touches[0].pageX)}
+        onTouchEnd={handleOnTouchEnd}
         style={isOpen ? openStyles : closeStyles}
         ref={burgerListRef}
         className="burger_list"
-      > 
-        <NavLink onClick={() => setIsOpen(false)} to="/">
+      >
+        <NavLink onClick={handleClickToLink} to="/">
           Главная
         </NavLink>
-        <NavLink onClick={() => setIsOpen(false)} to="/settings">
+        <NavLink onClick={handleClickToLink} to="/settings">
           Настройки
         </NavLink>
       </ul>
