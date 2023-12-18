f0887022aede4135a45571569c738b61ff28c7e5
Zmrts <julesik.zar@yandex.ru>
2023-12-18 15:17:05 +0400

    test styles for mobile2

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -2,7 +2,7 @@
 function Loader() {
 
     return <div style={{display:'flex', zIndex:'10',position:'fixed'}} className="overlay">
-         <span className="loader"></span>
+         <span className="preloader"></span>
     </div>
 }
 
